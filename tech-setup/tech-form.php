7d342a9e6247a9e91d43fa843874fe89dbf65930
Nico Watine <contact@studioncreations.com>
2015-11-27 11:16:59 -0600

    Computer OS DOWN!!

--- tech-setup/tech-form.php
+++ tech-setup/tech-form.php
@@ -3,7 +3,8 @@ $ToEmail = 'nico@studioncreations.com'; //ex. admin@scriptgates.com or scriptgat
 $EmailSubject = "Client Tech Stack Survey"."\n"; 
 $mailheader = "From: ".$_POST["email"]."\n";
 $mailheader .= "Reply-To: ".$_POST["email"]."\n";  
-$MESSAGE_BODY = "Name: " . $_POST["name"]; 
+$MESSAGE_BODY = "Name: " . $_POST["name"] . "\n"; 
+$MESSAGE_BODY .= "Computer OS:   " . ($_POST["computer-os-mac"]) . ($_POST["computer-os-windows"]) . ($_POST["computer-os-linux"]); 
 mail($ToEmail, $EmailSubject, $MESSAGE_BODY, $mailheader) or die ("Error!"); 
 
 header("Location: thanks.php");

e3594961f9fbb9f8d11eba3effe8f716d772be8d
Nico Watine <contact@studioncreations.com>
2015-11-27 11:21:07 -0600

    Linux version DOWN!

--- _site/tech-setup/tech-form.php
+++ _site/tech-setup/tech-form.php
@@ -6,7 +6,8 @@ $mailheader .= "Reply-To: ".$_POST["email"]."\n";
 $MESSAGE_BODY = "Name: " . $_POST["name"] . "\n"; 
 $MESSAGE_BODY .= "Computer OS:   " . ($_POST["computer-os-mac"]) . ($_POST["computer-os-windows"]) . ($_POST["computer-os-linux"]) . "\n"; 
 $MESSAGE_BODY .= "Windows Version:   " . $_POST["windows-xp"] . $_POST["windows-vista"] . $_POST["windows-7"] . $_POST["windows-8"] . $_POST["windows-8-1"] . $_POST["windows-10"] . $_POST["windows-unsure"] . "\n";
-$MESSAGE_BODY .= "Mac OS X Version:   " . $_POST["snow-leopard"] . $_POST["lion"] . $_POST["mountain-lion"] . $_POST["mavericks"] . $_POST["yosemite"] . $_POST["el-capitan"] . $_POST["mac-unsure"];
+$MESSAGE_BODY .= "Mac OS X Version:   " . $_POST["snow-leopard"] . $_POST["lion"] . $_POST["mountain-lion"] . $_POST["mavericks"] . $_POST["yosemite"] . $_POST["el-capitan"] . $_POST["mac-unsure"] . "\n";
+$MESSAGE_BODY .= "Linux Distro:   ".($_POST["ubuntu"]).($_POST["fedora"]).($_POST["linux-other"]); 
 mail($ToEmail, $EmailSubject, $MESSAGE_BODY, $mailheader) or die ("Error!"); 
 
 header("Location: thanks.php");
